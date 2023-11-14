d5242c6ff897faccdb7a38264c6058ecae1df989
Saicharan <saicharans@cogniswitch.ai>
2023-11-14 14:04:06 +0530

    added the example notebooks of llama index and llama hub with cogniswitch
    added example notebooks
    added source code for toolspec and query engine

---
+++ llamaswitch/__init__.py
@@ -0,0 +1,7 @@
+from llamaswitch.cognitool.base import CogniswitchToolSpec
+from llamaswitch.query_engine.cogniswitch_query_engine import CogniswitchQueryEngine
+
+__all__ = [
+    CogniswitchToolSpec,
+    CogniswitchQueryEngine
+]
